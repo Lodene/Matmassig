5b79180b0bd8536eafa617885f47900b945c3b3c
Antoine <aa@isdea.fr>
2025-01-08 15:11:32 +0100

    feat: add rabbitMq

---
+++ BackEnd/Matmassig/recipe-service/src/main/java/com/school/matmassig/recipeservice/config/ObjectMapperConfig.java
@@ -0,0 +1,14 @@
+package com.school.matmassig.recipeservice.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ObjectMapperConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
+}
