076c7e3275b99a5ca73bf285695bdd28c3f92f6f
Chatelet Ferrety Matias <matias.chatelet-ferrety@etu.uni-lyon1.fr>
2025-01-08 15:09:57 +0100

    fix: :bug: Update DTO of Recipe in Backend

--- BackEnd/Matmassig/orchestrator/src/main/java/com/school/matmassig/orchestrator/model/Recipe.java
+++ BackEnd/Matmassig/orchestrator/src/main/java/com/school/matmassig/orchestrator/model/Recipe.java
@@ -1,22 +1,16 @@
 package com.school.matmassig.orchestrator.model;
 
+import java.util.List;
+
 public class Recipe {
-    private Integer id;
     private String title;
     private String description;
     private Integer ingredientsRecipeId;
     private String instructions;
     private Integer userId;
+    private List<Integer> listIngredients;
 
     // Getters et Setters
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
     public String getTitle() {
         return title;
     }
@@ -55,5 +49,13 @@ public class Recipe {
 
     public void setUserId(Integer userId) {
         this.userId = userId;
+    }
+
+    public List<Integer> getListIngredients() {
+        return listIngredients;
+    }
+
+    public void setListIngredients(List<Integer> listIngredients) {
+        this.listIngredients = listIngredients;
     }
 }
