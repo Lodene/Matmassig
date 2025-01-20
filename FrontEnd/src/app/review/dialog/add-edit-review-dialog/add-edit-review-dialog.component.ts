52c17d32b03a349af03cdfbcf7d5b46210b11ceb
T-H-O-M-A-S <marin.f.thomas@gmail.com>
2025-01-20 13:32:09 +0100

    feat: review modal & ngx search select

---
+++ FrontEnd/src/app/review/dialog/add-edit-review-dialog/add-edit-review-dialog.component.ts
@@ -0,0 +1,122 @@
+import { Component, OnInit, Inject, ViewChild, AfterViewInit} from '@angular/core';
+import {
+  MAT_DIALOG_DATA,
+  MatDialog,
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogModule,
+  MatDialogRef,
+  MatDialogTitle,
+} from '@angular/material/dialog';
+import {MatSelect, MatSelectModule} from '@angular/material/select';
+import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
+import { NgxMatSelectSearchModule } from 'ngx-mat-select-search';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import { CommonModule } from '@angular/common';
+import { ReplaySubject, Subject, take, takeUntil } from 'rxjs';
+
+
+
+
+@Component({
+  selector: 'app-add-edit-review-dialog',
+  templateUrl: './add-edit-review-dialog.component.html',
+  styleUrls: ['./add-edit-review-dialog.component.scss'],
+  imports: [ReactiveFormsModule, NgxMatSelectSearchModule, CommonModule, FormsModule, MatDialogModule, MatSelectModule, MatButtonModule, MatFormFieldModule, MatInputModule]
+})
+export class AddEditReviewDialogComponent  implements OnInit, AfterViewInit {
+
+  /** list of banks */
+  protected recipes: any[] = [
+  'Recette 1',
+  'Recette 2',
+  'Recette 3',
+  'Recette 4'
+  ];
+
+  /** control for the selected bank */
+  public RecipeFormControl: FormControl<any> = new FormControl<any>(null);
+
+  /** control for the MatSelect filter keyword */
+  public RecipeFilterCtrl: FormControl<any> = new FormControl<string>('');
+
+  /** list of banks filtered by search keyword */
+  public filteredRecipes: ReplaySubject<any[]> = new ReplaySubject<any[]>(1);
+
+  @ViewChild('singleSelect', { static: true })
+  singleSelect!: MatSelect;
+
+  /** Subject that emits when the component has been destroyed. */
+  protected _onDestroy = new Subject<void>();
+
+
+  selectedRecipes = [...this.recipes];
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data: {name: string}) { }
+
+  ngOnInit() {
+    this.RecipeFormControl.setValue(this.recipes);
+    this.filteredRecipes.next(this.recipes.slice());
+    this.RecipeFilterCtrl.valueChanges
+    .pipe(takeUntil(this._onDestroy))
+    .subscribe(() => {
+      this.filterRecipe();
+    });
+  }
+  ngAfterViewInit() {
+    this.setInitialValue();
+  }
+
+  protected setInitialValue() {
+    this.filteredRecipes
+      .pipe(take(1), takeUntil(this._onDestroy))
+      .subscribe(() => {
+        // setting the compareWith property to a comparison function
+        // triggers initializing the selection according to the initial value of
+        // the form control (i.e. _initializeSelection())
+        // this needs to be done after the filteredBanks are loaded initially
+        // and after the mat-option elements are available
+        this.singleSelect.compareWith = (a: any, b: any) => a && b && a.id === b.id;
+      });
+  }
+
+  protected filterRecipe() {
+    console.log('filtering...')
+    if (!this.recipes) {
+      return;
+    }
+    // get the search keyword
+    let search = this.RecipeFilterCtrl.value;
+    console.log(search);
+    if (!search) {
+      this.filteredRecipes.next(this.recipes.slice());
+      return;
+    } else {
+      search = search.toLowerCase();
+    }
+    // filter the banks
+    this.filteredRecipes.next(
+      this.recipes.filter(recipe => recipe.toLowerCase().indexOf(search) > -1)
+    );
+  }
+
+  
+  onKey(value: any) { 
+    this.selectedRecipes = this.search(value.target.value as string);
+  }
+
+  triggerEvent(event: any) {
+    console.log(event);
+    if (this.selectedRecipes.length === 0) {
+      this.selectedRecipes = [...this.recipes];
+    }
+  }
+
+  search(value: string) {
+    let filter = value.toLowerCase();
+  return this.recipes.filter(option => option.toLowerCase().includes(filter));
+  }
+}
