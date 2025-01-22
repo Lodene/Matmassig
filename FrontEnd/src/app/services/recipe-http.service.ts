9bca361c9afda233eab0163328d540e4bdb070f1
Thomas0507 <marin.f.thomas@gmail.com>
2025-01-22 20:47:05 +0100

    feat: test bearer token

--- FrontEnd/src/app/services/recipe-http.service.ts
+++ FrontEnd/src/app/services/recipe-http.service.ts
@@ -3,21 +3,21 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RecipeHttpService {
-
-  constructor(private http: HttpClient) {
-
-  }
+  constructor(private http: HttpClient) {}
 
   getRecipeByUser(userId: string): Observable<any> {
-        const options = {
-          headers: new HttpHeaders({
-            'Content-Type': 'application/json',
-          })
-        };
-        return this.http.get('http://localhost:8080/api/orchestrator/recipe/getbyuser/' + userId);
+    const token = localStorage.getItem('auth-user');
+    const options = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      }),
+    };
+    return this.http.get(
+      'http://localhost:8080/api/orchestrator/recipe/getbyuser/' + userId
+    );
   }
-
 }
